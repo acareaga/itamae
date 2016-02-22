ccf4e2455f367080232157f781372acf6f012955
acareaga <aaroncareaga@gmail.com>
2016-02-22 13:12:10 -0700

    Updated app to present status of class. Starting file show and index features

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,7 @@
 import DS from 'ember-data';
 
 const electron = requireNode('electron');
-const remote = electron.remote;
-const mainProcess = remote.require('./electron');
+const mainProcess = electron.remote.require('./electron');
 const filesystem = mainProcess.filesystem;
 
 export default DS.Adapter.extend({
@@ -10,8 +9,5 @@ export default DS.Adapter.extend({
   findAll() {
     return filesystem.all();
   }
-
-  // find() {
-  // }
 
 });
